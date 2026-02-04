91b74d0b6e550647bba327ea51116560f36c8907
Entiti <ntthanh112005@gmail.com>
2026-02-04 19:13:55 +0700

    Add files via upload
    Server lan 1

---
+++ Server/Server/GameConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server
+{
+    public class GameConfig
+    {
+        public int BoardSize { get; set; }
+        public int WinCount { get; set; }
+        public int TurnTime { get; set; }
+
+        private static Dictionary<int, int> StandardRules = new Dictionary<int, int>
+        {
+            { 9, 4 },
+            { 10, 4 },
+            { 13, 5 },
+            { 15, 5 },
+            { 19, 5 },
+            { 20, 5 }
+        };
+
+        public GameConfig(int boardSize = 15, int? winCount = null, int turnTime = 30)
+        {
+            BoardSize = boardSize;
+            WinCount = winCount ?? GetDefaultWinCount(boardSize);
+            TurnTime = turnTime;
+
+            if (!IsValid())
+            {
+                throw new ArgumentException("Cấu hình game không hợp lệ!");
+            }
+        }
+
+        private int GetDefaultWinCount(int size)
+        {
+            if (StandardRules.ContainsKey(size))
+                return StandardRules[size];
+
+            if (size <= 10) return 4;
+            if (size <= 20) return 5;
+            return 6;
+        }
+
+        public bool IsValid()
+        {
+            return BoardSize >= 5 &&
+                   BoardSize <= 50 &&
+                   WinCount >= 3 &&
+                   WinCount <= BoardSize - 2 &&
+                   TurnTime >= 5;
+        }
+
+        public override string ToString()
+        {
+            return $"{BoardSize}x{BoardSize}, {WinCount} quân, {TurnTime}s";
+        }
+    }
+}
---
+++ Server/Server/Room.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using Newtonsoft.Json;
+
+namespace Server
+{
+    public class Room
+    {
+        public string RoomId { get; set; }
+        public List<Player> Players { get; set; } = new List<Player>();
+        public GameConfig Config { get; set; }
+        public char[,] Board;
+
+        public int TimeLeft;
+        public Timer TurnTimer;
+        public Player CurrentTurn;
+
+        private Action<Room, string> onLog;
+        private Action<Room, Player> onTimeout;
+
+        public Room(string id, GameConfig config, Action<Room, string> logCallback, Action<Room, Player> timeoutCallback)
+        {
+            RoomId = id;
+            Config = config;
+            onLog = logCallback;
+            onTimeout = timeoutCallback;
+            Board = new char[Config.BoardSize, Config.BoardSize];
+            TimeLeft = Config.TurnTime;
+
+            TurnTimer = new Timer(1000);
+            TurnTimer.Elapsed += TurnTimer_Elapsed;
+        }
+
+        void TurnTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            TimeLeft--;
+            BroadcastTimeLeft();
+
+            if (TimeLeft <= 0)
+            {
+                onLog?.Invoke(this, $"{CurrentTurn.Name} hết giờ!");
+                onTimeout?.Invoke(this, CurrentTurn);
+                SwitchTurn();
+            }
+        }
+
+        public bool IsFull() => Players.Count >= 2;
+
+        public void AddPlayer(Player p)
+        {
+            if (!IsFull())
+            {
+                Players.Add(p);
+                p.CurrentRoom = this;
+            }
+        }
+
+        public bool MakeMove(Player p, int x, int y)
+        {
+            if (p != CurrentTurn) return false;
+
+            if (x < 0 || x >= Config.BoardSize || y < 0 || y >= Config.BoardSize)
+                return false;
+
+            if (Board[x, y] != '\0')
+                return false;
+
+            Board[x, y] = p.Symbol[0];
+
+            onLog?.Invoke(this, $"{p.Name} đánh ({x},{y})");
+
+            TimeLeft = Config.TurnTime;
+            SwitchTurn();
+
+            return true;
+        }
+
+        public void SwitchTurn()
+        {
+            TimeLeft = Config.TurnTime;
+            CurrentTurn = Players.Find(p => p != CurrentTurn);
+            BroadcastTurn();
+        }
+
+        void BroadcastTurn()
+        {
+            var obj = new
+            {
+                type = "turn",
+                data = new
+                {
+                    currentPlayer = CurrentTurn.Name,
+                    symbol = CurrentTurn.Symbol,
+                    timeLeft = TimeLeft
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(obj);
+
+            foreach (var p in Players)
+                p.Send(json);
+        }
+
+        void BroadcastTimeLeft()
+        {
+            var obj = new
+            {
+                type = "time",
+                data = new { timeLeft = TimeLeft }
+            };
+
+            string json = JsonConvert.SerializeObject(obj);
+
+            foreach (var p in Players)
+                p.Send(json);
+        }
+
+        public bool CheckWin(int x, int y, string symbol)
+        {
+            char mark = symbol[0];
+            return CheckDirection(x, y, mark, 1, 0)
+                || CheckDirection(x, y, mark, 0, 1)
+                || CheckDirection(x, y, mark, 1, 1)
+                || CheckDirection(x, y, mark, 1, -1);
+        }
+
+        bool CheckDirection(int x, int y, char mark, int dx, int dy)
+        {
+            int count = 1;
+            count += Count(x, y, mark, dx, dy);
+            count += Count(x, y, mark, -dx, -dy);
+
+            return count >= Config.WinCount;
+        }
+
+        int Count(int x, int y, char mark, int dx, int dy)
+        {
+            int c = 0;
+            int i = x + dx;
+            int j = y + dy;
+
+            while (i >= 0 && i < Config.BoardSize &&
+                   j >= 0 && j < Config.BoardSize &&
+                   Board[i, j] == mark)
+            {
+                c++;
+                i += dx;
+                j += dy;
+            }
+            return c;
+        }
+
+        public void Stop()
+        {
+            TurnTimer?.Stop();
+            TurnTimer?.Dispose();
+        }
+    }
+}
